3bf349c2c190b36a81cdc435f4fe67c2a9128168
DeeFromNextDoor <131259901+DeewhyCodes@users.noreply.github.com>
2024-02-05 18:21:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-bg": "url('/cybersecurity-hero.jpg')",
+      },
+      fontFamily: {
+        sans: [
+          "sans-serif-font",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+      fontSize: {
+        base: "1rem",
+        xl: "1.25rem",
+      },
+      colors: {
+        "blue-main": "#00ADEF",
+        "red-main": "#C82C2F",
+      },
+    },
+  },
+  plugins: [],
+};
